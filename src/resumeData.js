c44376a616d79c8d634002c4d88d14aa08aecf13
omichandnani <chan8860@mylaurier.ca>
2019-06-10 19:51:01 -0400

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,112 @@
+let resumeData = {
+  "imagebaseurl":"https://github.com/ochandnani/Portfolio.git",
+  "name": "Omi Chandnani",
+  "role": "FullStack Developer",
+  "linkedinId":"https://www.linkedin.com/in/omi-chandnani-7292a5107/",
+  "skypeid": "Your skypeid",
+  "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+  "socialLinks":[
+      {
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/in/omi-chandnani-7292a5107/",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"https://github.com/omichandnani",
+        "className":"fa fa-github"
+      },
+      {
+        "name":"skype",
+        "url":"http://skype.com",
+        "className":"fa fa-skype"
+      }
+    ],
+  "aboutme":"I am doing my Masters in Applied Computing at Wilfrid Laurier University and have 3 years of experience as Full Stack Developer for Financial Company.",
+  "address":"Waterloo, ON Canada",
+  "website":"https://www.codeclassifiers.com",
+  "education":[
+    {
+      "UniversityName":"Wilfrid Laurier University",
+      "specialization":"Masters of Applied Computing",
+      "MonthOfPassing":"April",
+      "YearOfPassing":"2020",
+      "Achievements":"Some Achievements"
+    },
+    {
+      "UniversityName":"Mumbai University",
+      "specialization":"Bachelors in Computer Engineering",
+      "MonthOfPassing":"June",
+      "YearOfPassing":"2016",
+      "Achievements":"Some Achievements"
+    },
+    {
+      "UniversityName":"Maharastra State Board of Technical Education",
+      "specialization":"Diploma in Information Technology",
+      "MonthOfPassing":"June",
+      "YearOfPassing":"2013",
+      "Achievements":"Some Achievements"
+    }
+  ],
+  "work":[
+    {
+      "CompanyName":"Deloitte Canada",
+      "Role":"Machine Learning Co-op",
+      "From":"May,2019 - Present ",
+      "Location":"Kitchener, ON" 
+    },
+    {
+      "CompanyName":"BNP Paribas",
+      "Role":"Some specialization",
+      "From":"Jan",
+      "Location":"Some Achievements"
+    }
+  ],
+  "skillsDescription":"Your skills here",
+  "skills":[
+    {
+      "skillname":"HTML5",
+      "skillname":"HTML6"
+    },
+    {
+      "skillname":"CSS"
+    },
+    {
+      "skillname":"Reactjs"
+    }
+  ],
+  "portfolio":[
+    {
+      "name":"project1",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/phone.jpg"
+    },
+    {
+      "name":"project2",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/project.jpg"
+    },
+    {
+      "name":"project3",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/project2.png"
+    },
+    {
+      "name":"project4",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/phone.jpg"
+    }
+  ],
+  "testimonials":[
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    },
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
